44161fe8e95e5c400ce37dc7b095e3d9803e89ab
denisan2875 <33a653847d7c63919d169321eec4a0ae6806ddad>
2020-03-14 16:51:23 -0700

    Commit 1

--- DPFibonacci.go
+++ DPFibonacci.go
@@ -10,8 +10,44 @@ func FibNaive(n int) int {
 	}
 }
 
+var memo [100]int
+
+func FibDPTopDown(n int) int {
+	r := 0
+
+	if memo[n] != 0 {
+		return memo[n]
+	}
+	if n == 1 || n == 2 {
+		r = 1
+	} else {
+		r = FibDPTopDown(n-1) + FibDPTopDown(n-2)
+	}
+	memo[n] = r
+	return r
+}
+
+func FibDPBottomUp(n int) int {
+	r := 0
+	var fib = make([]int, n+1) //new([n]int)
+
+	for i := 1; i <= n; i++ {
+		if i <= 2 {
+			r = 1
+		} else {
+			r = fib[i-1] + fib[i-2]
+		}
+		fib[i] = r
+	}
+	return fib[n]
+}
+
 func main() {
 
-	fmt.Print("The result of Function Fibonacci (Naive Version) is: " + fmt.Sprint(FibNaive(10)))
+	n := 45
+
+	fmt.Print("The result of Function Fibonacci (Naive Version) is: " + fmt.Sprint(FibNaive(n)) + "\n")
+	fmt.Print("The result of Function Fibonacci (Dynamic Programing - Memoization (Top Down)) is: " + fmt.Sprint(FibDPTopDown(n)) + "\n")
+	fmt.Print("The result of Function Fibonacci (Dynamic Programing - Bottom Up) is: " + fmt.Sprint(FibDPBottomUp(n)) + "\n")
 
 }
